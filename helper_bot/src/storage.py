609cbeee9dbd6a65b128c54aac26def907a83e49
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2024-01-17 18:18:54 +0200

    fix path

--- helper_bot/src/storage.py
+++ helper_bot/src/storage.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
-package_path = os.path.dirname(sys.modules['__main__'].__file__)
-NOTES_FILE_NAME = os.path.join(package_path, 'notes_data.bin')
-CONTACTS_FILE_NAME = os.path.join(package_path, 'storage.bin')
+#package_path = os.path.dirname(sys.modules['__main__'].__file__)
+package_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+NOTES_FILE_NAME = os.path.join(package_path, r'notes_data.bin')
+CONTACTS_FILE_NAME = os.path.join(package_path, r'storage.bin')
